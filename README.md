584b2378c5c9cb12294148419f0db1c3b529cdbd
Eric Guimarães de Sousa Silva <eric.gssilva@gmail.com>
2019-07-12 13:46:54 -0300

    Initial project commit

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # csv-to-yaml
+
 Converting a transposed CSV to YAML
+
+## About
+
+This is very basic Shell Script thar converts a transposed CSV file to a beautiful YAML ;)
+
+## Dependencies
+
+I guess you only need `bash`.
+
+## Example
+
+The `csv-to-yaml.sh` script works in a very simple way. You just need to pass the path of a CSV file as the first argument of the script, and the output will a YAML file with the same name of the CSV, but with the `.yml` extention instead of `.csv`.
+
+### Input
+
+The input file is a **transposed CSV**. This repository includes a sample CSV file named `sample.csv`, which looks like this:
+
+```csv
+computer.specs.cpu.cores,4
+computer.specs.cpu.clock,1000GHz
+computer.specs.memory.slots,4
+computer.specs.memory.max,32Gb
+computer.price,US$499
+computer.brand,Generic Brand
+```
+
+### Execution
+
+The script is executed like this:
+
+```bash
+./csv-to-yaml.sh sample.csv
+```
+
+### Output
+
+The output will be a file named `sample.yml` in the same directory. The file will look like this:
+
+```yaml
+---
+computer:
+  brand: Generic Brand
+  price: US$499
+  specs:
+    cpu:
+      clock: 1000GHz
+      cores: 4
+    memory:
+      max: 32Gb
+      slots: 4
+```
+
+___
+
+Enjoy!
---
+++ csv-to-yaml.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# Options
+optTempFile="/tmp/csv-to-yaml.tmp"
+
+# First argument must be an CSV file
+if [[ $1 ]]; then
+  if [[ -f $1 ]]; then
+    echo "OK - valid file"
+  else
+    echo "File not found"
+    exit 1
+  fi
+else
+  echo "Missing file argument"
+  exit 1
+fi
+
+# Saving the file to a variable
+inputFile=$1
+outputFile=`echo "${inputFile}" | sed s/'.csv'/'.yml'/g`
+
+echo "Input file => ${inputFile}"
+echo "Output file => ${outputFile}"
+
+# The temp files must be empty
+> ${optTempFile}.1
+> ${optTempFile}.2
+
+# The CSV file must be alphabetically ordered
+sort ${inputFile} >> ${optTempFile}.1
+
+# Writing the initial YAML syntax
+echo "---" >> "${optTempFile}.2"
+
+# Loop through the lines
+IFS=$'\n'
+for line in `cat ${optTempFile}.1`; do
+  IFS=$'\n'
+
+  echo "Linha => ${line}"
+
+  key=`echo ${line} | cut -d, -f1`
+  value=`echo ${line} | cut -d, -f2`
+
+  echo "Chave => ${key}"
+  echo "Valor => ${value}"
+
+  # Counting the number of elements in key
+  elements=`echo "${key}" | tr "." "\n" | wc -l`
+  echo "Elementos => ${elements}"
+
+  # Loop though key elements
+  IFS="."
+  identation="" # Identation starts in zero
+  level=1 # Level starts in one
+  unset value_if_any # Unseting the value flag
+  for element in ${key}; do
+    echo "Elemento (${level}) => ${element}"
+
+    # Checking if the element is already in the file
+    if [[ `grep -e "^${identation}${element}:" "${optTempFile}.2"` ]]; then
+      # Line found - skipping
+      true
+
+    else
+      # Line not found - writing
+      # Checking if the element is the last (to append the value)
+      if [[ ${level} -eq ${elements} ]]; then
+        value_if_any=" ${value}"
+      fi
+
+      # Finally writing the line
+      echo "${identation}${element}:${value_if_any}" >> "${optTempFile}.2"
+
+    fi
+
+    # Preparing the next iteration
+    identation="${identation}  "
+    ((level++))
+
+  done
+
+done
+
+unset IFS
+
+# Wow! I think this worked. Lets save the file
+echo "Saving the final YAML to ${outputFile}"
+cp "${optTempFile}.2" "${outputFile}"
+
+# Cleaning the mess
+rm "${optTempFile}.1"
+rm "${optTempFile}.2"
+
+# The End, my friend.
+echo "CSV file converted (I guess)"; echo
+cat "${outputFile}"; echo
